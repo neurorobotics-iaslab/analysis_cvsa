af8ad109b8a995ada13645e9f68b612e97264cbf
PaoloFori <forin.paolo98@gmail.com>
2026-01-16 10:58:44 +0100

    correction bug on the for loop for the coninue value with eog empty

--- equal_ros/artifact_rejection.m
+++ equal_ros/artifact_rejection.m
@@ -112,7 +112,7 @@ for i=1:nchunks
     end
 
     % vertical and horizontal movements
-    if ~isempty(eog.label)
+    if ~isempty(eog.label) && size(eog_idx, 2) > 0
         heog = buffer_eog(:, eog_idx(1)) - buffer_eog(:, eog_idx(2));
         if size(eog_idx, 2) == 2
             % we have only FP1 and FP2
